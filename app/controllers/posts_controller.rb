90cda62451a0ee61830ad138ee70d90be807ee8b
two_app <wangyuliangf@gmail.com>
2017-01-13 23:19:00 +0800

    implement my post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,6 +7,17 @@ class PostsController < ApplicationController
     @post = Post.new
   end
 
+  def show
+    @group = Group.find(params[:group_id])
+    @post = Post.find(params[:id])
+  end
+
+  def edit
+    @group = Group.find(params[:group_id])
+    @post = Post.find(params[:id])
+  end
+
+
   def create
     @group = Group.find(params[:group_id])
     @post = Post.new(post_params)
@@ -20,11 +31,32 @@ class PostsController < ApplicationController
     end
   end
 
+    def update
+      @group = Group.find(params[:group_id])
+      @post = Post.find(params[:id])
+      @post.group = @group
+      @post.user = current_user
+      if @post.update(post_params)
+        redirect_to group_path(@group), notic: 'Upadate Success'
+      else
+        render :edit
+      end
+    end
+
+    def destroy
+      @group = Group.find(params[:group_id])
+      @post = Post.find(params[:id])
+      @post.group = @group
+      @post.user = current_user
+
+      @post.destroy
+      redirect_to group_path(@group), alert: 'Group deleted'
+    end
+
 
   private
 
   def post_params
     params.require(:post).permit(:content)
   end
-
 end
